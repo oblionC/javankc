5e0b0d67749d415eab7998e5a39ba5c52b76bb06
Tanmay Kule <tanmaythegreat17@gmail.com>
2022-12-19 14:53:49 +0530

    multtable completed

---
+++ multtable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class multtable {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+        for(int i = 1; i <= 10; i++) {
+            result += n;
+            System.out.printf("%d x %d = %d\n", n, i, result);
+        }
+    }
+}
